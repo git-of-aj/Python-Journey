c0adaea90d9d52b86f92282df8b0bc6e6fb3ba65
Ananay Ojha <76782360+Ananyojha@users.noreply.github.com>
2022-06-19 17:30:43 +0530

    Create operators.py

---
+++ HackerRank/Easy/operators.py
@@ -0,0 +1,8 @@
+'''
+
+'''
+# take multiple inputs ; split them by whitespaces ; int can map only one input at a time so use map for multiple inputs
+x,y=map(int,input().split())
+print(f'{x+y} \n{x*y} \n{x-y}')
+# print without specifying variables 
+#print(f'{x+y},{x*y},{x-y}')
