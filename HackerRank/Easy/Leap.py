308afbf6f4a74b0113028948edd8a30cc1a4dcb1
Ananay Ojha <76782360+Ananyojha@users.noreply.github.com>
2022-06-20 20:17:58 +0530

    Update Leap.py

--- HackerRank/Easy/Leap.py
+++ HackerRank/Easy/Leap.py
@@ -1,5 +1,13 @@
 # any year divisible by 400 and 4 is a leap year
 
 year = int(input())
+#return year
 
 # creating a function is_leap to identify leap year from given input
+
+def is_leap() :
+    if year/4 == 0 :
+        if year/400 == 0 :
+            print(f'{year} is a leap year')
+    else :
+    print(f'{year} is not a leap year')
