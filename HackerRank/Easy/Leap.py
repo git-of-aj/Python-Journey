aad546df5084a1db7e86b7d283a85a40d6881766
Ananay Ojha <76782360+Ananyojha@users.noreply.github.com>
2022-06-20 20:25:50 +0530

    Update Leap.py

--- HackerRank/Easy/Leap.py
+++ HackerRank/Easy/Leap.py
@@ -6,8 +6,21 @@ year = int(input())
 # creating a function is_leap to identify leap year from given input
 
 def is_leap() :
-    if year/4 == 0 :
-        if year/400 == 0 :
+    if year%4 == 0 :
+        if year%400 == 0 :
             print(f'{year} is a leap year')
     else :
     print(f'{year} is not a leap year')
+
+# --------------
+
+# 2 method
+
+year = int(input())
+return year
+
+def is_leap(year) :
+    if (year%4 == 0) or (year%400 == 0) :
+        print('year is a leap year')
+    else :
+        print('Not a leap year')
