519c4da304004d8367ffc6d363ee621ecb817779
Ananay Ojha <76782360+Ananyojha@users.noreply.github.com>
2022-10-10 10:49:16 +0530

    Create Lists-learning-1.py

---
+++ Codes/Simple/Lists-learning-1.py
@@ -0,0 +1,34 @@
+# Lists makes your program look elegant
+# helps to remove repitive Code
+
+'''
+print('Enter the name of cat 1:')
+catName1 = input()
+print('Enter the name of cat 2:')
+catName2 = input()
+print('Enter the name of cat 3:')
+catName3 = input()
+print('Enter the name of cat 4:')
+catName4 = input()
+print('Enter the name of cat 5:')
+catName5 = input()
+print('Enter the name of cat 6:')
+catName6 = input()
+print('The cat names are:')
+print(catName1 + ' ' + catName2 + ' ' + catName3 + ' ' + catName4 + ' ' +
+catName5 + ' ' + catName6)
+'''
+
+# with use of list, the above programs 👇
+
+catNames = []
+while True:
+    print('Enter the name of cat ' + str(len(catNames) + 1) +
+      ' (Or enter nothing to stop.):')
+    name = input()
+    if name == '':
+        break
+    catNames = catNames + [name]  # list concatenation
+print('The cat names are:')
+for name in catNames:
+    print('  ' + name)
