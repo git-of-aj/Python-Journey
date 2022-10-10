7c653e4c716ec1d13e9e11f2790784782a13f4f4
Ananay Ojha <76782360+Ananyojha@users.noreply.github.com>
2022-10-10 10:52:44 +0530

    Update Lists-learning-1.py

--- Codes/Simple/Lists-learning-1.py
+++ Codes/Simple/Lists-learning-1.py
@@ -32,3 +32,19 @@ while True:
 print('The cat names are:')
 for name in catNames:
     print('  ' + name)
+
+# -------------------------
+
+# use lists like a Pro 👍🏻
+
+supplies = ['pens', 'staplers', 'flamethrowers', 'binders']
+>>> for i in range(len(supplies)):
+...     print('Index ' + str(i) + ' in supplies is: ' + supplies[i])
+
+# expected output : 
+"""
+Index 0 in supplies is: pens
+Index 1 in supplies is: staplers
+Index 2 in supplies is: flamethrowers
+Index 3 in supplies is: binders
+"""
