907efe50aebf6d6a18fc4af1d0b58e0808d1b132
Ananay Ojha <76782360+Ananyojha@users.noreply.github.com>
2023-01-17 00:47:22 +0530

    Update hello.py

--- HackerRank/Easy/hello.py
+++ HackerRank/Easy/hello.py
@@ -2,3 +2,12 @@
 link : `https://www.hackerrank.com/challenges/py-hello-world/problem?isFullScreen=true`
 '''
  print "Hello, World!"
+
+ # LEVEL 2
+ 
+ # Python evaluates from left to right
+ # TAke input --> remove whitespaces from left --> Captilize first letter (title) -> seperate them using white space
+ name1,name2 = input('Please enter your name: ').lstrip().title().split(' ')
+ 
+ # print f with format
+print(f"Nice to meet you {name1} with surname {name2}")
