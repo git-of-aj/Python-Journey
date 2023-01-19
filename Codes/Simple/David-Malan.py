45388e877081e26fe56f0f316050fb01ef1aed9d
Ananay Ojha <76782360+Ananyojha@users.noreply.github.com>
2023-01-19 18:24:48 +0530

    Update David-Malan.py

--- Codes/Simple/David-Malan.py
+++ Codes/Simple/David-Malan.py
@@ -8,3 +8,13 @@ hello(name)
 
 # User Flow: Name variable takes input --> Hands over that input to Friend Variable.
 # if you replace hello(something-else) --> NameError coz nothing specified
+#-----------------------------------------------------------------------------------------------------------
+
+# say hello
+def hello(friend):
+    friend = input("Hi 👋, What's Your name: ")
+    print("Hello,",friend,sep='')
+# call that main
+hello(friend = "World") # specifying argument here.... instead of inside function
+# if you delete input .. Prints Hello World
+# more: https://www.freecodecamp.org/news/python-function-examples-how-to-declare-and-invoke-with-parameters-2/
