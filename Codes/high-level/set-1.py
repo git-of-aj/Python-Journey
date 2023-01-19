9a01059fe9614c5973e8c48dca84a9644ed0564a
Ananay Ojha <76782360+Ananyojha@users.noreply.github.com>
2023-01-19 18:04:31 +0530

    Create set-1.py

---
+++ Codes/high-level/set-1.py
@@ -0,0 +1,11 @@
+x = float(input("What's x? "))
+y = float(input("What's y? "))
+
+z = x/y
+# rounds off to 2 decimal places like output from below: 
+# 0.88 not 0.875
+print(f"after dividing we found - {z:.2f}")
+
+# ---------------------------------------------------------------------------------------------------------------------
+
+
