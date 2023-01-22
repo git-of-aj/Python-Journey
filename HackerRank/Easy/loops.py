10d8b3fb281b27723194ec903a7f604df26324c3
Ananay Ojha <76782360+Ananyojha@users.noreply.github.com>
2023-01-22 14:22:18 +0530

    Update loops.py

--- HackerRank/Easy/loops.py
+++ HackerRank/Easy/loops.py
@@ -9,3 +9,19 @@ if x > 0 :
         print(x^2)
 else 
 print('condition void')
+
+
+
+
+#--------------------------------
+
+# https://www.hackerrank.com/challenges/python-loops/problem?isFullScreen=true&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+
+# take input 
+n= int(input())
+# i is a variable which takes value [0,1,...n-1]
+# iterate till  i < n and sqaure 
+for i in range(n):
+    if i < n:
+        print(i*i)
+        # or print(i^2)
