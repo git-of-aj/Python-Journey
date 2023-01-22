f2c235f82fb7cac63e9c9e4791cee2311478a604
Ananay Ojha <76782360+Ananyojha@users.noreply.github.com>
2023-01-22 14:00:32 +0530

    Update operators.py

--- HackerRank/Easy/operators.py
+++ HackerRank/Easy/operators.py
@@ -3,6 +3,11 @@
 '''
 # take multiple inputs ; split them by whitespaces ; int can map only one input at a time so use map for multiple inputs
 
+# version2 
+x,y=map(int,input("Please enter 2 numbers, seperated with comma(,): ").split(','))
+print(f'{x+y} \n{x*y} \n{x-y}')
+
+# version1 
 x,y=map(int,input().split())
 print(f'{x+y} \n{x*y} \n{x-y}')
 
