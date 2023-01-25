c0bf817198f1915927216129ace4fbec7190dde7
Ananay Ojha <76782360+Ananyojha@users.noreply.github.com>
2023-01-25 16:42:36 +0530

    Create swap.py

---
+++ HackerRank/Easy/swap.py
@@ -0,0 +1,18 @@
+def swap_case(s):
+    SwAp = ''
+    # one by one take characters present in s
+    for char in s:
+        # if the picked character is not alreday in upper case..
+        if char.upper() != char:
+            SwAp += char.upper() 
+            # this makes it upper case & adds it to
+            # the empty string... 
+        else:
+            char.lower() != char
+            SwAp += char.lower()
+    return SwAp
+
+if __name__ == '__main__':
+    s = input()
+    result = swap_case(s)
+    print(result)
