011fdbaa80019bcefaf84b48c96d3c4a58c50464
Ananay Ojha <76782360+Ananyojha@users.noreply.github.com>
2023-02-05 17:52:41 +0530

    Create find_percentage.py

---
+++ HackerRank/Easy/find_percentage.py
@@ -0,0 +1,26 @@
+if __name__ == '__main__':
+    n = int(input("Number: "))
+    student_marks = {}
+    total = 0
+    for _ in range(n):
+        name, *line = input("name then score by space: ").split()
+        # * variable returns list of inputs (unlimited inputs) confirm by --> print(line)
+        #name then score by space: mangu 1 23 76 78
+    # ['1', '23', '76', '78']
+        scores = list(map(float, line))
+        student_marks[name] = scores
+    query_name = input("Name to find average: ")
+    print(student_marks)
+
+    query_list = student_marks[query_name]
+
+    for num in query_list:
+        total = total + num 
+
+    avg = total / len(query_list)
+      
+        
+    print(f"The Average marks for {query_name} is {avg:.2f}")
+    # avg: .2f means round it off to 2 decimal places.
+
+
