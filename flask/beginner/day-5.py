dc74036af9e43810dbba0150fb67844f37085033
Ananay Ojha <76782360+Ananyojha@users.noreply.github.com>
2023-04-02 23:52:34 +0530

    template inheritance 01-base.html

--- flask/beginner/day-5.py
+++ flask/beginner/day-5.py
@@ -19,3 +19,36 @@ JINJA TEMPLATE BASICS:
   
 # template inheritance = you want same features, methods acroos all pages like a navigation bar, header, footer then create a base template, import these settings from base
 # to child template 
+
+  # --------------------------------------------------------------------------------------------------------------------------------------
+  
+  # app.py file to have template inheritance
+  
+  from flask import Flask, url_for , render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return render_template("base.html",link=url_for('child'))
+
+@app.route("/child")
+def child():
+    return render_template("child.html")
+
+@app.route("/<name>")
+def hi(name):
+    letters_list = list(name)
+    length = len(letters_list)
+    letters_dict = {'letter1': letters_list[0],'letter2': letters_list[1],
+                    'start-2-end': f"there start 2 end {letters_list[0:]}"}
+    # return render_template("filename under template folder", variables)
+    return render_template("index.html", name = name, alphabets = letters_list, 
+                           key_value = letters_dict,len = length)
+
+with app.test_request_context():
+  # this prints output to test in the terminal
+    print(url_for('child'))
+
+if __name__ == "__main__":
+    app.run(debug=True)
